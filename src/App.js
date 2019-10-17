76f092c07fb621066bab474d7ae9fd1c147f1b99
ZiiQooS <ahmedzakariaesdairi@gmail.com>
2019-10-17 12:26:28 +0100

    Create common components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import {Header, Footer} from "./components/common";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header>Header</Header>
+            <Button variant="contained" color="primary">
+                Hello World
+            </Button>
+            <Footer>Footer</Footer>
+        </div>
+    );
 }
 
 export default App;
